65719afe1a4a44ba2bb8bf09238fdabe540536f4
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2023-03-16 16:25:17 +0000

    Notification service - v1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# notification-service
-Notification Service
+# Notification Service
+
+## Instruction to run this service
+
+- 1 - Stay in folder notservice
+- 2 - To test sms/email go to index file and set your phone number and your email address
+- 3 - In the same folder run node index.js
+- 4 - Type in you browser http://localhost:3010/v1/notifications/sms and see in your mobile the message.
---
+++ notservice/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const port = 3010;
+
+const dotenv = require("dotenv").config();
+if (dotenv.error) {
+  console.error("Error occurred while setting dot env files : ", dotenv.error);
+}
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const client = require("twilio")(accountSid, authToken);
+
+app.get("/v1/notifications/sms", (req, res) => {
+  client.messages
+    .create({
+      body: "Hi there, EGS Project",
+      from: phoneNumber,
+      to: "+351xxxxxx",
+    })
+    .then((message) => {
+      console.log(message.sid);
+      res.json({ sid: message.sid }).end();
+      return;
+    })
+    .catch((err) => {
+      res.json(error).end();
+      return;
+    });
+});
+
+//send e-mail message
+const sgMail = require("@sendgrid/mail");
+app.get("/v1/notifications/email", (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "xxxx@outlook.com",
+    from: "xxxx@outlook.com",
+    subject: "EGS Project | Notification Service",
+    text: "We notify you about your appointment",
+    html: "<strong>Prapare your documents and get ready for it.</strong>",
+  };
+  sgMail.send(msg);
+  console.log(`Email sent to ${msg.to}`);
+});
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
