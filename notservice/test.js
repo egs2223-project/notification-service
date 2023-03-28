43404e805e1bef3cfef1504fced778d3278e93b0
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2023-03-28 21:11:54 +0100

    Notification Service - v3

--- notservice/test.js
+++ notservice/test.js
@@ -9,26 +9,26 @@
 // .then(response => response.json())
 // .then(response => console.log(JSON.stringify(response)))
 
-const fetch = require("node-fetch");
-fetch("http://localhost:3030/v1/notifications/email", {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    send_to: "jmuachifi@gmail.com",
-    subject: "Test Subject",
-    text: "Text",
-    html: "Html",
-    attachments: [
-      {
-        content: "asas".toString("base64"),
-        filename: "attachment.pdf",
-        type: "application/pdf",
-      },
-    ],
-  }),
-})
-  .then((response) => response.json())
-  .then((response) => console.log(JSON.stringify(response)));
+//const fetch = require("node-fetch");
+//fetch("http://localhost:3030/v1/notifications/email", {
+//  method: "POST",
+//  headers: {
+//    Accept: "application/json",
+//    "Content-Type": "application/json",
+//  },
+//  body: JSON.stringify({
+//    send_to: "jmuachifi@gmail.com",
+//    subject: "Test Subject",
+//    text: "Text",
+//    html: "Html",
+//    attachments: [
+//      {
+//        content: "asas".toString("base64"),
+//        filename: "attachment.pdf",
+//        type: "application/pdf",
+//      },
+//    ],
+//  }),
+//})
+//  .then((response) => response.json())
+//  .then((response) => console.log(JSON.stringify(response)));
